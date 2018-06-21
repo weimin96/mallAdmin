dbccf98eac2426c5ee5931efd65c9f6f83b0dff5
weimin96 <735313582@qq.com>
2018-06-21 13:38:40 +0800

    初始化

---
+++ src/component/page-title/index.jsx
@@ -0,0 +1,28 @@
+/*
+* @Author: areo
+* @Date:   2017-02-11 20:06:59
+* @Last Modified by:   areo
+* @Last Modified time: 2017-02-24 21:06:43
+*/
+
+'use strict';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const PageTitle = React.createClass({
+    componentDidMount(){
+        document.title = this.props.pageTitle || '天喵 Admin'
+    },
+    render() {
+        return (
+            <div className="row">
+                <div className="col-lg-12">
+                    <h1 className="page-header">{this.props.pageTitle}</h1>
+                    {this.props.children}
+                </div>
+            </div>
+        );
+    }
+});
+
+export default PageTitle;
